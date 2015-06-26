10c78b50ee5aae767ec6422c3952752c86237d6d
Ofer83 <oferraviv83@gmail.com>
2015-06-26 20:05:49 +0300

    ofer1

--- FunPaint/src/graphicshape/Segment.java
+++ FunPaint/src/graphicshape/Segment.java
@@ -6,7 +6,6 @@ package graphicshape;
 public class Segment {
     private Point p1;
     private Point p2;
-
     /*homework:
     add constractor + clone add seterqgeter (no alias/pointer)
      add method length of segment
@@ -14,4 +13,24 @@ public class Segment {
        - override the method equals and tostring
        - method return the slope of line (deltay/deltax)
      */
+    public Segment(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+    public Segment(Segment segment){
+        this(segment.getP1(),segment.getP2());
+    }
+    public Point getP1() {
+        return p1;
+    }
+    public Point getP2() {
+        return p2;
+    }
+    public void setP1(Point p1) {
+        //this.p1 = p1;
+        this.p1 = new Point(p1);
+    }
+    public void setP2(Point p2) {
+        this.p2 = new Point(p2);
+    }
 }
